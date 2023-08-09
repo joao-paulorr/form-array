56804df413638c15ee240514dd92584f08a05e70
João P <132589767+joao-paulorr@users.noreply.github.com>
2023-08-09 14:21:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function login() {
+    var user = document.querySelector('#user').value
+    var pass = document.querySelector('#password').value
+
+    // Array
+    var usuarios = [
+        'Eduardo',
+        'admin',
+        'rodrigo'
+    ]
+
+    var senhas = [
+        '123456',
+        'Umdoistres45!',
+        'singed'
+    ]
+
+    // minusculo
+    var usuario = user.toLowerCase()
+
+    if (user == '' || senha == '') {
+        alert('Usuário ou senha em branco!')
+    } else {
+        for
+    }
+}
