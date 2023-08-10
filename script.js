d4d681db80c4bec3f66fc4d54608543e25a08fc5
João P <132589767+joao-paulorr@users.noreply.github.com>
2023-08-10 11:37:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function login() {
     var user = document.querySelector('#user').value
     var pass = document.querySelector('#password').value
+    var controle = false
 
     // Array
     var usuarios = [
@@ -11,16 +12,23 @@ function login() {
 
     var senhas = [
         '123456',
-        'Umdoistres45!',
+        'adminstr',
         'singed'
     ]
 
     // minusculo
-    var usuario = user.toLowerCase()
+    // var usuario = user.toLowerCase()
 
-    if (user == '' || senha == '') {
+    if (user == '' || pass == '') {
         alert('Usuário ou senha em branco!')
     } else {
-        for
+        for (i = 0; i < usuarios.length; i++) {
+            if (user == usuarios[i] && pass == senhas[i]) {
+                controle = true
+                location.href=('paginainicial.html')
+            }
+        } if (controle == false) {
+            alert('Usuário ou senha incorretos!')
+        }
     }
 }
